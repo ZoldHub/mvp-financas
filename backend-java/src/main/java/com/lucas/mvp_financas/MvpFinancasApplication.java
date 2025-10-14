a3a6cc37a531a215554f139959572be27103b2ce
Lucas Fernandes <lucas_13icem@hotmail.com>
2025-10-13 22:16:24 -0300

    Estrutura do spring boot do backend java

---
+++ backend-java/src/main/java/com/lucas/mvp_financas/MvpFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.mvp_financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpFinancasApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MvpFinancasApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/lucas/mvp_financas/MvpFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.mvp_financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
