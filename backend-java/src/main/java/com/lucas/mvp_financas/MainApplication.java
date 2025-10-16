ad43c93b81b53c606354b8f157fdb93f399181bc
Lucas Fernandes <lucas_13icem@hotmail.com>
2025-10-16 15:14:49 -0300

    Ajustando backend, alterando a classe padrão main

--- backend-java/src/main/java/com/lucas/mvp_financas/MainApplication.java
+++ backend-java/src/main/java/com/lucas/mvp_financas/MainApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvpFinancasApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvpFinancasApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
-
 }
