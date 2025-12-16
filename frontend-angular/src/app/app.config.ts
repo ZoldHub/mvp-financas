5688faf48474eccdc45bf749cf1f5cc0a758a472
Lucas Fernandes <lucas_13icem@hotmail.com>
2025-12-15 23:14:29 -0300

    Downgrade do angular 21 para versão 20 e instalação da biblioteca ng-zorro

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { pt_PT, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideNzI18n(pt_PT), provideAnimationsAsync(), provideHttpClient()
   ]
 };
